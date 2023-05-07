7ef19004cae49d6ce939e2d1db1f9ee65ac4ec26
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-07 14:07:15 +0200

    test brushless

--- rp-robotics-rust/src/main.rs
+++ rp-robotics-rust/src/main.rs
@@ -17,13 +17,13 @@ use rppal::pwm::{Channel, Polarity, Pwm};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
-    let pwm = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+    let pwm = Pwm::with_frequency(Channel::Pwm0, 100.0, 0.25, Polarity::Normal, true)?;
 
     // Sleep for 2 seconds while the LED blinks.
     thread::sleep(Duration::from_secs(2));
 
     // Reconfigure the PWM channel for an 8 Hz frequency, 50% duty cycle.
-    pwm.set_frequency(8.0, 0.5)?;
+    pwm.set_frequency(1000.0, 0.5)?;
 
     thread::sleep(Duration::from_secs(3));
 

1fe5ed9c6002c2218926dd2bcba1370562962a24
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-07 14:01:00 +0200

    test brushless

--- rp-robotics-rust/src/main.rs
+++ rp-robotics-rust/src/main.rs
@@ -1,4 +1,4 @@
-use rp_robotics_rust::led;
+/*use rp_robotics_rust::led;
 use rp_robotics_rust::ultrasonic;
 
 fn main() {
@@ -6,4 +6,29 @@ fn main() {
     //led::launch().unwrap()
     ultrasonic::launch().unwrap()
 
+}
+*/
+
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::pwm::{Channel, Polarity, Pwm};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Enable PWM channel 0 (BCM GPIO 18, physical pin 12) at 2 Hz with a 25% duty cycle.
+    let pwm = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+
+    // Sleep for 2 seconds while the LED blinks.
+    thread::sleep(Duration::from_secs(2));
+
+    // Reconfigure the PWM channel for an 8 Hz frequency, 50% duty cycle.
+    pwm.set_frequency(8.0, 0.5)?;
+
+    thread::sleep(Duration::from_secs(3));
+
+    Ok(())
+
+    // When the pwm variable goes out of scope, the PWM channel is automatically disabled.
+    // You can manually disable the channel by calling the Pwm::disable() method.
 }

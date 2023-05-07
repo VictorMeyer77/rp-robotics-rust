5b716f07e3d8367a13422be39bd9052ca6046e13
Victor-Algo <victor.algo.contact@gmail.com>
2023-05-07 15:04:32 +0200

    test brushless 2

--- tmp-test.py
+++ tmp-test.py
@@ -18,18 +18,18 @@ p.ChangeDutyCycle(3)
 print("3")
 time.sleep(5)
 
-i = 4
+ii = 4
 steps = [5.85, 5.9, 5.95, 6.0]
 current_step = -1
 
-def increase(target):
+def increase(i, target):
     while i < target:
         print(i)
         p.ChangeDutyCycle(i)
         time.sleep(0.05)
         i += 0.02
 
-def decrease(target):
+def decrease(i, target):
     while i > target:
         print(i)
         p.ChangeDutyCycle(i)
@@ -41,9 +41,11 @@ while True:
     print(s)
     if s == "a" and current_step < 3:
        current_step += 1
-       increase(steps[current_step])
+       increase(ii, steps[current_step])
+       ii = steps[current_step]
     elif s == "z" and current_step > 0:
         current_step -= 1
-        decrease(steps[current_step])
+        decrease(ii, steps[current_step])
+        ii = steps[current_step]
     elif s == "p":
         break
